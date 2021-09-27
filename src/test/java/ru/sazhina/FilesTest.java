81cd4c851b0da61486b20e4cf37124f2d57cb2f6
Elena <Lena-Sazh@users.noreply.github.com>
2021-09-27 23:23:14 +0300

    Files handling examples

---
+++ src/test/java/ru/sazhina/FilesTest.java
@@ -0,0 +1,103 @@
+package ru.sazhina;
+
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
+import net.lingala.zip4j.core.ZipFile;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FilesTest {
+
+    @Test
+    void parseTXT() throws Exception {
+        String parsed;
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("txt.txt")) {
+            assert is != null;
+            parsed = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+            assertThat(parsed).contains("Test file .txt");
+        }
+    }
+
+    @Test
+    void parsePDF() throws Exception {
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("pdf.pdf")) {
+            assert is != null;
+            PDF parsed = new PDF(is);
+            assertThat(parsed.text).contains("Test file .pdf");
+        }
+    }
+
+    @Test
+    void parseCSV() throws Exception {
+        URL url = getClass().getClassLoader().getResource("csv.csv");
+        assert url != null;
+        CSVReader parsed = new CSVReader(new FileReader(new File(url.toURI())));
+
+            List<String[]> strings = parsed.readAll();
+
+            assertThat(strings).contains(
+                new String[] {"CSV1", "11"},
+                new String[] {"CSV2", "22"},
+                new String[] {"CSV3", "33"}
+        );
+    }
+
+    @Test
+    void parseXLS() throws Exception {
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("xls.xls")) {
+            assert is != null;
+            XLS parsed = new XLS(is);
+            assertThat(parsed.excel.getSheetAt(0).getRow(0).getCell(0).getStringCellValue())
+                    .isEqualTo("TEST");
+            assertThat(parsed.excel.getSheetAt(0).getRow(0).getCell(1).getStringCellValue())
+                    .isEqualTo("test");
+        }
+    }
+
+    @Test
+    void parseZIP() throws Exception {
+        ZipFile zipFile = new ZipFile("./src/test/resources/zip.zip");
+
+        if (zipFile.isEncrypted()) {
+            zipFile.setPassword("12345");
+        }
+        zipFile.extractAll("./src/test/resources/extracted");
+
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("txt.txt")) {
+            assert is != null;
+            String parsed = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+            assertThat(parsed).contains("Test file .txt");
+        }
+
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("xls.xls")) {
+            assert is != null;
+            XLS parsed = new XLS(is);
+            assertThat(parsed.excel.getSheetAt(0).getRow(0).getCell(0).getStringCellValue())
+                    .isEqualTo("TEST");
+            assertThat(parsed.excel.getSheetAt(0).getRow(0).getCell(1).getStringCellValue())
+                    .isEqualTo("test");
+        }
+    }
+
+    @Test
+    void parseDOCX() throws Exception {
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("docx.docx")) {
+            assert is != null;
+            XWPFDocument file = new XWPFDocument(is);
+            XWPFWordExtractor xwpfWordExtractor = new XWPFWordExtractor(file);
+            String docText = xwpfWordExtractor.getText();
+            assertThat(docText.contains("Test file .docx"));
+        }
+    }
+}
